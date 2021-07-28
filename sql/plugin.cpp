a1dedf12f2804a319a26b574a52a7c4f3e329e6b
brad medeiros <bradmedeiros0@gmail.com>
2021-07-27 23:24:55 -0700

    hook up fns

--- sql/plugin.cpp
+++ sql/plugin.cpp
@@ -1,76 +1,26 @@
 #include <iostream>
 #include <libguile.h>
 #include "./sql.h"
-/*
 
-std::string mainCommand = scm_to_locale_string(scm_list_ref(sqlQuery, scm_from_int64(0)));
-  std::string table = scm_to_locale_string(scm_list_ref(sqlQuery, scm_from_int64(1)));
-  SqlQuery query {
-    .type = SQL_SELECT,
-    .table = table,
-  };
-  if (mainCommand == "select"){
-    std::cout << "setting query to select" << std::endl;
-    query.type = SQL_SELECT;
-    query.queryData = SqlSelect{
-      .columns = listToVecString(scm_list_ref(sqlQuery, scm_from_int64(2))),
-      .filter = SqlFilter {
-        .hasFilter = false,
-        .column = "",
-        .value = "",
-        .invert = false,
-      }
-    };
-  }else if (mainCommand == "insert"){
-    query.type = SQL_INSERT;
-    query.queryData = SqlInsert {
-      .columns = listToVecString(scm_list_ref(sqlQuery, scm_from_int64(2))),
-      .values = listToVecString(scm_list_ref(sqlQuery, scm_from_int64(3))),
-    };
-  }else if (mainCommand == "update"){
-    query.type = SQL_UPDATE;
-    std::cout << "WARNING: GENERIC UPDATE, NOT USING ACTUAL VALUES" << std::endl;
-    query.queryData = SqlUpdate {
-      .columns = { "name" },
-      .values = { "no-one" },
-      .filter = SqlFilter {
-        .hasFilter = true,
-        .column = "name",
-        .value = "unknown",
-        .invert = false,
-      }
-    };
-  }else if (mainCommand == "delete"){
-    query.type = SQL_DELETE;
-    std::cout << "WARNING: GENERIC DELETE, NOT USING ACTUAL VALUES" << std::endl;
-    query.queryData = SqlDelete {
-      .filter = SqlFilter {
-        .hasFilter = true,
-        .column = "description",
-        .value = "hello",
-        .invert = false,
-      }
-    };
-  }else if (mainCommand == "create-table"){
-    std::cout << "setting query to create" << std::endl;
-    query.type = SQL_CREATE_TABLE;
-    query.queryData = SqlCreate{
-      .columns = listToVecString(scm_list_ref(sqlQuery, scm_from_int64(2))),
-    };
-  }else if (mainCommand == "delete-table"){
-    query.type = SQL_DELETE_TABLE;
+SCM nestedVecToSCM(std::vector<std::vector<std::string>>& list){
+  SCM scmList = scm_make_list(scm_from_unsigned_integer(list.size()), scm_from_unsigned_integer(0));
+  for (int i = 0; i < list.size(); i++){
+    auto sublist = list.at(i);
+    SCM scmSubList = scm_make_list(scm_from_unsigned_integer(sublist.size()), scm_from_unsigned_integer(0));
+    for (int j = 0; j < sublist.size(); j++){
+      scm_list_set_x (scmSubList, scm_from_unsigned_integer(j), scm_from_locale_string(sublist.at(j).c_str()));
+    }
+    scm_list_set_x(scmList, scm_from_unsigned_integer(i), scmSubList);
   }
-  std::cout << "INFO: executing sql query" << std::endl;
-  auto sqlResponse = executeSqlQuery(query);
-  std::cout << "INFO: finished executing query" << std::endl;
-  return listToSCM(sqlResponse);*/
+  return scmList;
+}
 
-
-/*struct sqlQueryHolder {
+struct sqlQueryHolder {
   SqlQuery* query;
 };
 
 SCM sqlObjectType;  
+
 SqlQuery* queryFromForeign(SCM sqlQuery){
   sqlQueryHolder* query;
   scm_assert_foreign_object_type(sqlObjectType, sqlQuery);
@@ -91,10 +41,11 @@ SCM scmSqlCompile(SCM sqlQueryString){
   queryObj -> query = query;
   return scm_make_foreign_object_1(sqlObjectType, queryObj);
 }
+
 void finalizeSqlObjectType(SCM sqlQuery){
   auto query = queryFromForeign(sqlQuery);
   delete query;
-}*/
+}
 
 
 SCM scmHelloWorld(){
@@ -104,14 +55,19 @@ SCM scmHelloWorld(){
 
 void registerGuileFns() asm ("registerGuileFns");
 void registerGuileFns() { 
- //scm_c_define_gsubr("sql", 0, 0, 0, (void*)test);
- // scm_c_define_gsubr("sql", 1, 0, 0, (void*)scmSql);
- // scm_c_define_gsubr("sql-compile", 1, 0, 0, (void*)scmSqlCompile);
+ scm_c_define_gsubr("sql", 1, 0, 0, (void*)scmSql);
+ scm_c_define_gsubr("sql-compile", 1, 0, 0, (void*)scmSqlCompile);
+}
+
+void registerGuileTypes() asm("registerGuileTypes");
+void registerGuileTypes(){
+  sqlObjectType = scm_make_foreign_object_type(scm_from_utf8_symbol("sqlquery"), scm_list_1(scm_from_utf8_symbol("data")), finalizeSqlObjectType);
 }
 
 #ifdef BINARY_MODE
 
 int main(){
+
   std::cout << "sql - hello world" << std::endl;
 }
 
