900886e1dc873b681f2820dbc58aff54a6031e20
brad medeiros <bradmedeiros0@gmail.com>
2021-07-27 19:16:53 -0700

    sample -> include example scheme fn

--- sample/plugin.cpp
+++ sample/plugin.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <libguile.h>
 
 void onStart() asm ("onStart");
 void onStart() { 
@@ -10,3 +10,21 @@ void onFrame() asm ("onFrame");
 void onFrame() { 
   std::cout << "hello world 2 from on start!" << std::endl;
 }
+
+SCM scmHelloWorld(){
+  std::cout << "hello world" << std::endl;
+  return SCM_UNSPECIFIED;
+}
+
+void registerGuileFns() asm ("registerGuileFns");
+void registerGuileFns() { 
+  scm_c_define_gsubr("hello-world", 0, 0, 0, (void *)scmHelloWorld);
+}
+
+#ifdef BINARY_MODE
+
+int main(){
+  scmHelloWorld();
+}
+
+#endif
